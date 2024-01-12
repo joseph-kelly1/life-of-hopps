6cd1459f15e0ad7d7c2d1c2fe5b352ab25fb9e97
Joseph Kelly <josephkelly1107@gmail.com>
2024-01-12 14:06:28 -0500

    added bg aseprite file to assets folder

--- hopps.py
+++ hopps.py
@@ -6,11 +6,18 @@ class Hopps:
     def __init__(self, image, pos):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
+        self.base_player_image = self.image
+        self.hitbox_rect = self.base_player_image.get_rect
+        self.rext = self.hitbox_rect.copy()
         self.pos = pos
+
+    def player_rotation(self):
+        pass
+
+    def move(self):
         self.vel_x = 0
         self.vel_y = 0
 
-    def move(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
