1ad642c920e6bdceafa9c706a6ff27bb56487539
tkiernan13 <128085647+tkiernan13@users.noreply.github.com>
2024-04-24 09:06:10 -0400

    Endless

---
+++ levels.py
@@ -0,0 +1,32 @@
+levels = [
+    {
+        # level 1
+        "spiders": 1,
+        "beetles": 1,
+        "stinkbugs": 1
+    },
+    {
+        # level 2
+        "spiders": 2,
+        "beetles": 2,
+        "stinkbugs": 2
+    },
+    {
+        # level 3
+        "spiders": 3,
+        "beetles": 3,
+        "stinkbugs": 3
+    },
+    {
+        # level 4
+        "spiders": 4,
+        "beetles": 4,
+        "stinkbugs": 4
+    },
+    {
+        # level 5
+        "spiders": 5,
+        "beetles": 5,
+        "stinkbugs": 5
+    }
+]
